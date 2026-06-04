7d4a66a199484390c19ae81f4dbff266ad266c89
MahaAswin <mahaaswinsb@gmail.com>
2026-06-04 09:46:38 +0530

    Modified AuthService

--- src/main/java/com/example/QuickBite/auth/service/AuthService.java
+++ src/main/java/com/example/QuickBite/auth/service/AuthService.java
@@ -2,9 +2,8 @@ package com.example.QuickBite.auth.service;
 
 import com.example.QuickBite.auth.dto.RegisterRequest;
 import com.example.QuickBite.auth.dto.loginRequest;
-import org.springframework.stereotype.Service;
 
-@Service
+
 public interface AuthService {
 
     public String register(RegisterRequest request);
